fa940f12b70f73d4e81368a642ef46eb30cfb0b7
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-04-12 16:21:59 -0700

    Add navbar and shop components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Homepage from "./components/Homepage";
+import Navbar from "./components/Navbar";
+import Shop from "./components/Shop";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Homepage />
     </div>
   );
