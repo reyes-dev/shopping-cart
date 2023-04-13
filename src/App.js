1c187d2827ec17cdc883e4de672224fa1387376f
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-04-12 18:32:13 -0700

    Remove unneeded imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
-import Homepage from "./components/Homepage";
 import Navbar from "./components/Navbar";
-import Shop from "./components/Shop";
 import { Outlet } from "react-router-dom";
-import RouteSwitch from "./RouteSwitch";
 
 function App() {
   return (
